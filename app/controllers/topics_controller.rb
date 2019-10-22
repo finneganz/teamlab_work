13076953f061d55eb6221a6576c544aa4d5c7c04
finneganz <abs0lutely.n0nsense.cs@gmail.com>
2019-10-22 18:23:46 +0900

    Add method at topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,15 +1,22 @@
 class TopicsController < ApplicationController
   def index
+    @topics = Topic.all
   end
 
   def new
   end
 
   def create
+    @topic = Topic.create(topic_params)
   end
 
   def destroy
   end
-  
+
+  protected
+
+  def topic_params
+    params.require(:topic).permit(:title)
+  end
 
 end
